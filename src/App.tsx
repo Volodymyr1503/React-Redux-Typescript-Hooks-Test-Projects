97d8324fe85db395ce974f81183640f4efdf056f
Volodymyr <volodymyr15.2017@gmail.com>
2021-11-23 17:41:47 +0200

    todo starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import TodoList from './components/TodoList';
-import UserList from './components/UserList';
 
 
 const App: React.FC = () => {
 
   return (
     <div className="wrapper">
-      {/* <UserList /> */}
-      <TodoList />
+
     </div>
   );
 }
